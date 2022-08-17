a1100f15ef97b79f805ef87b315f69fe8c264117
Vaishnav-novo <Vaishnav@novo.co>
2022-08-17 17:43:58 +0530

    Initial Commit

---
+++ SplitItSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SplitItSwiftUI
+//
+//  Created by VaishnavChidambar on 17/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SplitItSwiftUI/SplitItSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitItSwiftUIApp.swift
+//  SplitItSwiftUI
+//
+//  Created by VaishnavChidambar on 17/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SplitItSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
